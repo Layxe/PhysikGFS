f5771f6471bf2ab0188a0deea97ec221c7d5ef6c
Alexander <alexander.niedermayer0@gmail.com>
2017-06-30 15:49:36 +0200

    Added wave system

--- src/js/Main.js
+++ src/js/Main.js
@@ -8,6 +8,18 @@
  *
  */
 
+// ### Globale Einstellungen ######################################## //
+
+const RESOLUTION = 2;
+
+// ### Variablen #################################################### //
+
+var oldTime = 0;
+var FPS     = 0;
+
+
+// ### Display ###################################################### //
+
 /**
  * Statisches Display
  * @param element canvas element
@@ -40,61 +52,309 @@ StDisplay.prototype.init = function init() {
 
 };
 
-StDisplay.prototype.drawPoint = function drawPoint(point) {
+/**
+ * Zeichne einen Punkt
+ * @param point
+ * @param amplitude Amplitude der Welle
+ * @param color Farbe des Punktes
+ */
 
-  this.ctx.fillStyle = 'black';
-  this.ctx.fillRect(point.x,250+Math.sin(point.angle)*250, 1, 1);
+StDisplay.prototype.drawPoint = function drawPoint(point, amplitude, color) {
+
+  var x = point.x;
+  var y = 250+Math.sin(point.angle) * amplitude;
+
+  this.ctx.fillStyle = color;
+  this.ctx.fillRect(x,y, 1, 1);
+
+  this.ctx.lineTo(x,y);
+
+
+};
+
+/**
+ * Zeichne eine Welle, die aus sich aus mehreren Wellen zusammensetzt
+ * @param waves
+ * @param color
+ */
+
+StDisplay.prototype.drawCombinedWave = function drawCombinedWave(waves, color) {
+
+  this.ctx.beginPath();
+  this.ctx.strokeStyle = color;
+
+  for(var k = 0; k < Display.getPoints(); k++) {
+
+    var still = false;
+    var x = k*RESOLUTION;
+    var y = 0;
+
+    for(var i = 0; i < waves.length; i++) {
+
+      y += Math.sin(waves[i].points[k].angle) * waves[i].amplitude;
+
+      if(waves[i].points[k].still) {
+        still = true;
+      }
+
+    }
+
+    y += 250;
+
+    // Falls nicht alle Punkte sich überlappen soll nicht gezeichnet werden
+    if(still) {
+      continue;
+    }
+
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(x,y, 1, 1);
+
+    this.ctx.lineTo(x,y);
+
+  }
+
+  this.ctx.stroke();
+  this.ctx.closePath();
+
+};
+
+StDisplay.prototype.drawInterface = function drawInterface() {
+
+  this.ctx.beginPath();
+  this.ctx.moveTo(0,250);
+  this.ctx.lineTo(Display.width, 250);
+  this.ctx.strokeStyle = 'black';
+  this.ctx.lineWidth = 2;
+  this.ctx.stroke();
+
+};
+
+StDisplay.prototype.getPoints = function getPoints() {
+
+  return Math.round(Display.width / RESOLUTION);
 
 };
 
 const Display = new StDisplay(document.getElementById('display'));
 
+// ### Welt ######################################################### //
 
 /**
- *
+ * Welt, die die Punkte beinhaltet
  * @constructor
  */
 
 function StWorld() {
 
-  this.points = new Array(3);
-
-  for(var i = 0; i < 3; i++) {
-    this.points[i] = new Array(Display.width / 2);
-
-    for(var k = 0; k < this.points[i].length; k++) {
-
-      this.points[i][k] = new Point(k*2, 0);
-
-    }
-
-  }
+  this.waves = [];
 
 }
 
-StWorld.prototype.loopPoints = function loopPoints(arrayIndex,callback) {
+/**
+ * Zeichne alle Punkte, die in der Welt existieren
+ */
 
-  for(var i = 0; i < this.points[arrayIndex].length; i++) {
+StWorld.prototype.drawWaves = function drawWaves() {
 
-    callback(this.points[arrayIndex][i]);
+  for(var i = 0; i < this.waves.length; i++) {
+
+    this.waves[i].draw();
 
   }
 
 };
 
-StWorld.prototype.drawPoints = function drawPoints() {
+/**
+ * Erstelle eine neue Welle
+ * @param c Ausbreitungsgeschwindigkeit
+ * @param frequency Frequenz
+ * @param amplitude Amplitude
+ * @param index Speicherort
+ */
 
-  for(var i = 0; i < 3; i++) {
+StWorld.prototype.createWave = function createWave(c,frequency,amplitude) {
 
-    for(var k = 0; k < this.points[i].length; k++) {
+  var wave = new Wave(c,frequency,amplitude);
+  World.waves.push(wave);
+  return wave;
 
-      Display.drawPoint(this.points[i][k]);
+};
+
+StWorld.prototype.createCombinedWave = function createCombinedWave(waves) {
+
+  var cw = new CombinedWave(waves);
+  World.waves.push(cw);
+  return cw;
+
+};
+
+/**
+ * Animiere alle Wellen
+ */
+
+StWorld.prototype.simulate = function simulate() {
+
+  for(var i = 0; i < this.waves.length; i++) {
+
+    if(this.waves[i] instanceof Wave)
+        World.waves[i].simulate();
+
+  }
+
+};
+
+// ### Welle ######################################################## //
+
+/** Wellenobjekt
+ * --------------------------
+ * Wird mit World.createWave() erstellt
+ * @param c         {number}
+ * @param frequency {number}
+ * @param amplitude {number}
+ * @constructor
+ */
+
+function Wave(c,frequency,amplitude) {
+
+  this.time = 0; // Relative Zeitphase der Welle
+  this.c = c;
+  this.frequency = frequency;
+  this.amplitude = amplitude;
+  this.phi = 3.14*2;
+
+  this.reverse = false; // Läuft die Welle nach rechts
+  this.running = false; // Wird die Welle fortlaufen simuliert
+
+  this.color = 'orange';
+  this.strokeWidth = 3;
+
+  this.points = new Array(Display.getPoints());
+
+  for(var i = 0; i < Display.getPoints(); i++) {
+
+    this.points[i] = new Point(i*RESOLUTION, 0);
+
+  }
+
+
+
+}
+
+/**
+ * Starte die Welle
+ */
+
+Wave.prototype.start = function start() {
+
+  this.running = true;
+
+};
+
+/**
+ * Stoppe die Welle
+ */
+
+Wave.prototype.stop = function stop() {
+
+  this.running = false;
+
+};
+
+/**
+ * Zeichne alle Punkte, die die Welle beinhaltet
+ */
+
+Wave.prototype.draw = function draw() {
+
+  Display.ctx.beginPath();
+  Display.ctx.lineWidth = this.strokeWidth;
+  Display.ctx.strokeStyle = this.color;
+
+  for(var i = 0; i < Display.getPoints(); i++) {
+
+    if(!this.points[i].still) // Falls der Punkt nicht schwingt, wird er auch nicht gezeichnet
+      Display.drawPoint(this.points[i], this.amplitude, this.color);
+
+  }
+
+  Display.ctx.stroke()
+
+};
+
+/**
+ * Setze die Zeit der Welle und aktualisiere die Position der Punkte
+ * @param time {number}
+ */
+
+Wave.prototype.setTime = function setTime(time) {
+
+  this.time = time;
+
+
+  for(var i = 0; i < this.points.length; i++) {
+
+    var point;
+
+    if(this.reverse) {
+
+      point = this.points[this.points.length - (i+1)];
+
+    } else {
+
+      point = this.points[i];
 
     }
 
+
+    point.angle = 0; // Setze den Winkel zurück, falls die Welle neugestartet wird
+
+    var T = 1 / this.frequency;
+    var lambda = this.c * T;
+
+    if (this.time * this.c >= i) {
+      point.angle = (2 * Math.PI * (this.time / T - i / lambda)) + this.phi; // Berechne den Winkel des Zeigers
+      point.still = false;
+    } else {
+      point.still = true;
+    }
+
+
   }
 
 };
+
+/**
+ * Simuliere die Fortbewegung der Welle indem die Zeit durchgehend
+ * erhöht wird
+ */
+
+Wave.prototype.simulate = function simulate() {
+
+  if(!this.running) {
+    return;
+  }
+
+  this.setTime(this.time);
+  this.time++;
+
+};
+
+/**
+ * Versetze die Welle in Ursprungsposition
+ */
+
+Wave.prototype.restart = function restart() {
+
+  this.time = 0;
+
+};
+
+/**
+ * Erstelle einen Punkt
+ * @param x
+ * @param y
+ * @constructor
+ */
 
 function Point(x,y) {
 
@@ -102,45 +362,80 @@ function Point(x,y) {
   this.y = y;
   this.angle = 0;
 
+  this.still = true;
+
 }
 
-Point.prototype.increase = function increase(value) {
+// ### Kombinierte Welle ############################################ //
 
-  this.angle += value;
-  if(this.angle >= 360) {
-    this.angle = this.angle - 360;
-  }
+function CombinedWave(waves) {
+
+  this.waves = waves;
+  this.color = 'green';
+  this.running = true;
+
+}
+
+CombinedWave.prototype.draw = function draw() {
+
+  Display.drawCombinedWave(this.waves, this.color);
 
 };
 
-var oldTime = 0;
-var FPS     = 0;
+// ### Nützliche Funktionen ######################################### //
+
+function resetEverything() {
+
+  for(var i = 0; i < 3; i++) {
+
+    World.waves[i].restart();
+
+  }
+
+}
+
+// ### Programmablauf ############################################### //
+
+/**
+ * Programmloop
+ * -----------------------
+ * Sorge dafür, dass die Wellen falls gewollt animiert
+ * und gezeichnet werden
+ */
 
 function loop() {
 
   Display.ctx.clearRect(0,0,Display.width,Display.height);
-  World.drawPoints();
 
-  World.loopPoints(0, function(point) {
-    point.increase(.05);
-  });
+  World.drawWaves();  // Zeichne die Punkte der Wellen
+  World.simulate();   // Aktualisiere die Wellen
 
-  // FPS Zähler
+  Display.drawInterface();
+
+  // ~~~ Messe die Bilder pro Sekunde ~~~ //
   FPS++;
   if(new Date().getTime() > oldTime + 1000) {
 
-    console.log(FPS);
     FPS = 0;
     oldTime = new Date().getTime();
 
   }
 
-
   window.requestAnimationFrame(loop);
+
 }
 
 Display.init();
 
 const World = new StWorld();
 
+World.createWave(2,0.005,100);
+World.createWave(1,0.005,100);
+World.waves[1].reverse = true;
+
+World.createCombinedWave([World.waves[0], World.waves[1]]);
+
 loop();
+
+World.waves[0].start();
+World.waves[1].start();
